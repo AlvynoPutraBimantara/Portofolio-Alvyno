28cd9a1887b4ff62bc7ed33fc078288585676cdd
Alvyno Putra Bimantara <alvyno.putra.b@gmail.com>
2026-05-20 10:59:46 +0700

    Build Ver. 1.0

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -383,7 +383,7 @@ function scrollToSection(id) {
 /* LAYOUT*/
 .dashboard {
   position: relative;
-  padding-right: 60px; /* space for side nav */
+  padding-right: 60px;
 }
 
 /* ════════════════════════════════════════
